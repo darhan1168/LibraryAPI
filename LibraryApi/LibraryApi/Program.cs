c52b44a870d3668dbf0b271902af99cc030bdd88
darhan1168 <darinahanina04@gmail.com>
2023-09-19 21:19:41 +0200

    Add services in Program

--- LibraryApi/LibraryApi/Program.cs
+++ LibraryApi/LibraryApi/Program.cs
@@ -1,9 +1,16 @@
+using LibraryApi.Repositories.Implementations;
+using LibraryApi.Repositories.Interfaces;
+using LibraryApi.Services.Implementations;
+using LibraryApi.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IPasswordService, PasswordService>();
+builder.Services.AddScoped<ISeedValuesService, SeedValuesService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
