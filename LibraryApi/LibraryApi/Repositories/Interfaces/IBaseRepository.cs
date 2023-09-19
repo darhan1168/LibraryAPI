f5da7725407e13e74a467eb853a5e485e56d04a2
darhan1168 <darinahanina04@gmail.com>
2023-09-19 21:16:57 +0200

    Update IBaseRepository.cs

--- LibraryApi/LibraryApi/Repositories/Interfaces/IBaseRepository.cs
+++ LibraryApi/LibraryApi/Repositories/Interfaces/IBaseRepository.cs
@@ -6,7 +6,7 @@ namespace LibraryApi.Repositories.Interfaces;
 public interface IBaseRepository<T> where T : BaseEntity
 {
     Task<T> GetById(int id);
-    Task<List<T>> GetAll();
+    List<T> GetAll();
     Task<Result<bool>> Add(T entity);
     Task<Result<bool>> Delete(T entity);
     Task<Result<bool>> Update(T entity);
