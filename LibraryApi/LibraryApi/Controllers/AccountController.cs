1f7fc910ceec4fb165df83da5a6759f05aa6817a
darhan1168 <darinahanina04@gmail.com>
2023-09-21 08:07:18 +0200

    Add Authentication

--- LibraryApi/LibraryApi/Controllers/AccountController.cs
+++ LibraryApi/LibraryApi/Controllers/AccountController.cs
@@ -1,7 +1,10 @@
+using System.Security.Claims;
 using LibraryApi.Helpers;
 using LibraryApi.Models;
 using LibraryApi.Services.Interfaces;
 using LibraryApi.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -39,6 +42,8 @@ public class AccountController : Controller
                 return BadRequest(new { Error = addingResult.Message });
             }
             
+            await Authenticate(model.Email);
+            
             return Ok();
         }
         
@@ -63,9 +68,31 @@ public class AccountController : Controller
                 return BadRequest(new { Error = loginResult.Message });
             }
             
+            await Authenticate(model.Email);
+            
             return Ok();
         }
         
         return BadRequest(new { Error = "Model is not valid" });
+    }
+    
+    [HttpPost("Logout")]
+    public async Task<IActionResult> Logout()
+    {
+        await HttpContext.SignOutAsync();
+        
+        return Ok();
+    }
+    
+    private async Task Authenticate(string email)
+    {
+        var claims = new List<Claim>
+        {
+            new (ClaimsIdentity.DefaultNameClaimType, email)
+        };
+
+        var id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+
+        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
     }
 }
--- LibraryApi/LibraryApi/Program.cs
+++ LibraryApi/LibraryApi/Program.cs
@@ -2,9 +2,16 @@ using LibraryApi.Repositories.Implementations;
 using LibraryApi.Repositories.Interfaces;
 using LibraryApi.Services.Implementations;
 using LibraryApi.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options => 
+    {
+        options.LoginPath = new PathString("/Account/Login");
+    });
 
 // Add services to the container.
 builder.Services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
@@ -18,7 +25,8 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<LibraryApi.AppContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseLazyLoadingProxies()
+        .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 var app = builder.Build();
@@ -32,6 +40,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();   
 app.UseAuthorization();
 
 app.MapControllers();
