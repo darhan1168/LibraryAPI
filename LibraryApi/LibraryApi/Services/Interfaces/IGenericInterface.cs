63e02df1498dcf0df6235cb3939c752135e07e80
darhan1168 <darinahanina04@gmail.com>
2023-09-21 09:43:22 +0200

    Delete Generic service

--- LibraryApi/LibraryApi/Services/Interfaces/IGenericInterface.cs
+++
@@ -1,9 +0,0 @@
-using LibraryApi.Models;
-using LibraryApi.Repositories.Interfaces;
-
-namespace LibraryApi.Services.Interfaces;
-
-public interface IGenericInterface<T> where T : BaseEntity
-{
-    Task<T>? GetById(int id);
-}
